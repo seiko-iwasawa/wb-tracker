ad2e41289b2b6f2db5461643f35d0b4d59dbfb5d
seiko.iwasawa <seiko.iwasawa@gmail.com>
2025-01-14 16:26:55 +0300

    add appdata.py

---
+++ wbtracker/appdata.py
@@ -0,0 +1,31 @@
+import pathlib
+import sys
+import shutil
+
+_home = pathlib.Path.home()
+"""Home path."""
+if sys.platform == "win32":
+    appdata = _home / "AppData/Roaming"
+elif sys.platform == "linux":
+    appdata = _home / ".local/share"
+elif sys.platform == "darwin":
+    appdata = _home / "Library/Application Support"
+else:
+    appdata = _home
+root = appdata / "WB Tracker"
+debug_root = pathlib.Path(".")
+
+DEBUG = True
+
+if DEBUG:
+    root = debug_root
+else:
+    if (
+        not root.exists()
+        or open(debug_root / "data" / "version.txt").readline()
+        != open(root / "data" / "version.txt").readline()
+    ):
+        if root.exists():
+            shutil.rmtree(root)
+        root.mkdir()
+        shutil.copytree(debug_root / "data", root / "data")
