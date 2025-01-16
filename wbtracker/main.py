6350ee0d15729f4ddc92237d0bda1c394d71a46f
seiko.iwasawa <seiko.iwasawa@gmail.com>
2025-01-17 00:03:38 +0300

    add info output field

--- wbtracker/main.py
+++ wbtracker/main.py
@@ -10,6 +10,10 @@ class MainWindow(win.Window):
         super().__init__(1080, 720, "WB Tracker")
         self.reg_obj(win.Background(self, (255, 255, 255)))
         self._reg_menu()
+        self._output = win.Text(
+            "output", self, win.Text.Label("", 30, 30, color=(0, 0, 0))
+        )
+        self.reg_obj(self._output)
 
     def _reg_menu(self) -> None:
         menu = win.WinBlock("menu", self)
@@ -25,6 +29,10 @@ class MainWindow(win.Window):
                 utils.add_products,
             )
         )
+
+    def _info(self, text: str):
+        self._output.text.text = text
+        self.on_draw()
 
 
 def main():
